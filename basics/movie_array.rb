12606ecf22a413e289216bcbaf7c1757faa100f2
Naveed Fida <naveedfida01@gmail.com>
2016-02-02 21:30:34 -0800

    First chapter completed

---
+++ basics/movie_array.rb
@@ -0,0 +1,8 @@
+
+years = [1979, 1984, 1990, 1993, 1999]
+
+puts years[0]
+puts years[1]
+puts years[2]
+puts years[3]
+puts years[4]
---
+++ basics/movie_titles.rb
@@ -0,0 +1,11 @@
+movies = { :alien => 1979,
+           :the_terminator => 1984,
+           :goodfellas => 1990,
+           :groundhog_day => 1993,
+           :american_beauty => 1999 }
+
+puts movies[:alien]
+puts movies[:the_terminator]
+puts movies[:goodfellas]
+puts movies[:groundhog_day]
+puts movies[:american_beauty]
